dcf58503a2e92322b25b48a66e99928e762c8d79
liux <liuxjobs@163.com>
2021-09-15 18:22:46 +0800

    hot-fix first commit

--- src/main/java/Controller/MyController.java
+++ src/main/java/Controller/MyController.java
@@ -3,5 +3,6 @@ package Controller;
 public class MyController {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hot-fix first");
     }
 }
