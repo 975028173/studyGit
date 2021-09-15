8adff90f61dea9936115190c2a93b129b9bf1879
liux <liuxjobs@163.com>
2021-09-15 18:31:17 +0800

    hot-fix second commit

--- src/main/java/Controller/MyController.java
+++ src/main/java/Controller/MyController.java
@@ -4,5 +4,6 @@ public class MyController {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hot-fix first");
+        System.out.println("hot-fix second");
     }
 }
