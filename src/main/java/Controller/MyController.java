56b13e6c4bc586159c248f7b871a70717e91b036
liux <liuxjobs@163.com>
2021-09-15 18:49:02 +0800

    hot-fix 3 commit

--- src/main/java/Controller/MyController.java
+++ src/main/java/Controller/MyController.java
@@ -5,5 +5,6 @@ public class MyController {
         System.out.println("hello git");
         System.out.println("hot-fix first");
         System.out.println("hot-fix second");
+        System.out.println("hot-fix 3");
     }
 }
