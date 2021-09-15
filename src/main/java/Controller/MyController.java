e6c562d4249d55ecb7ed70e304760edb243c194e
975028173 <45868181+975028173@users.noreply.github.com>
2021-09-15 18:57:22 +0800

    Update MyController.java

--- src/main/java/Controller/MyController.java
+++ src/main/java/Controller/MyController.java
@@ -7,5 +7,6 @@ public class MyController {
         System.out.println("master second");
         System.out.println("hot-fix second");
         System.out.println("hot-fix 3");
+        System.out.println("master 3");
     }
 }
